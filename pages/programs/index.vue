394612cd5128b246691870b00e2e8b1237d423c0
Jismayadi <jismayadi@gmail.com>
2021-06-19 13:17:41 +0800

    adding owl-carousel

--- pages/programs/index.vue
+++ pages/programs/index.vue
@@ -1,86 +1,65 @@
 <template>
-<b-container>
-  <b-row class="mt-5">
-    <b-col cols="12" class="mt-5">
-      <Public-Cards-Programs />
-    </b-col>
+  <b-container>
+    <b-row class="mt-5">
+      <b-col cols="12" class="mt-5">
+        <Public-Cards-Programs />
+      </b-col>
 
-    <b-col cols="12">
-      <div class="text-center mt-5">
-      <h2>Media</h2>
-      <hr style="height: 5px; width: 100px" class="bg-azure" />
-    </div>
-    
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1900"
-      img-height="600"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
+      <b-col cols="12">
+        <div class="text-center mt-5 mb-4">
+          <h2>Media</h2>
+          <hr style="height: 5px; width: 100px" class="bg-azure" />
+        </div>
 
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
-    </b-carousel>
-    </b-col>
-  </b-row>
-</b-container>
+        <div class="owl-carousel owl-theme">
+          <img src="https://placeimg.com/1600/800/any?1" />
+          <img src="https://placeimg.com/1600/800/any?2" />
+          <img src="https://placeimg.com/1600/800/any?3" />
+          <img src="https://placeimg.com/1600/800/any?4" />
+          <img src="https://placeimg.com/1600/800/any?5" />
+          <img src="https://placeimg.com/1600/800/any?6" />
+          <img src="https://placeimg.com/1600/800/any?7" />
+          <img src="https://placeimg.com/1600/800/any?8" />
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        slide: 0,
-        sliding: null
-      }
-    },
-    methods: {
-      onSlideStart(slide) {
-        this.sliding = true
-      },
-      onSlideEnd(slide) {
-        this.sliding = false
-      }
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
     }
-  }
+  },
+  mounted() {
+    this.initCarousel()
+  },
+  methods: {
+    initCarousel() {
+      $('.owl-carousel').owlCarousel({
+        responsiveClass: true,
+        margin: 10,
+        responsive: {
+          0: {
+            items: 1,
+            nav: true,
+          },
+          600: {
+            items: 3,
+            nav: false,
+          },
+        },
+      })
+    },
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    },
+  },
+}
 </script>
