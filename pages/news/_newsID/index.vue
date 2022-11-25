bdaa4a1a62da3458e569407d738335ef72ebb9ab
Jismayadi <jismayadi@gmail.com>
2022-11-25 20:45:23 +0800

    init

--- pages/news/_newsID/index.vue
+++ pages/news/_newsID/index.vue
@@ -1,148 +1,130 @@
 <template>
-    <v-container fluid class="pa-0">
-        <v-img lazy-src="https://cdn.tmpo.co/data/2020/02/07/id_913062/913062_720.jpg" max-height="500" max-width="2000"
-            aspect-ratio="1" src="https://cdn.tmpo.co/data/2020/02/07/id_913062/913062_720.jpg">
-        </v-img>
+  <v-container fluid class="pa-0">
+    <v-img
+      lazy-src="https://cdn.tmpo.co/data/2020/02/07/id_913062/913062_720.jpg"
+      max-height="500"
+      max-width="2000"
+      aspect-ratio="1"
+      src="https://cdn.tmpo.co/data/2020/02/07/id_913062/913062_720.jpg"
+    >
+    </v-img>
 
-        <v-row class="mb-10">
-            <v-col cols="12" class="mb-5">
-                <div class="text-center mt-10 mb-10">
-                    <h1>Berita</h1>
-                    <hr style="width:150px; margin: auto;" class="mt-1 teal" />
-                </div>
-                <v-row class="mx-5">
-                    <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
-                        <v-card>
-                            <v-img :src="card.src" class="white--text align-end"
-                                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px">
-                                <v-card-title v-text="card.title"></v-card-title>
-                            </v-img>
+    <v-row class="mb-10 mx-10">
+      <v-col>
+        <h2 class="blue--text mt-10 mb-1">
+          Wali Kota Rencanakan Vaksinasi Tahap I Termin II Dilaksanakan Pekan 4
+        </h2>
+        <h4 class="mb-10">Kamis, 24-11-2022</h4>
 
-                            <v-card-title> Top western road trips </v-card-title>
+        <p align="justify" class="text-wrap">
+          Satgas Covid-19 Kota Balikpapan menerima informasi bahwa Rabu
+          (24/2/21) besok akan tiba vaksin untuk Kalimantan Timur (Kaltim) di
+          Bandara Sepinggan. Diantaranya, ada jatah untuk Kota Balikpapan. Wali
+          Kota Balikpapan, Rizal Effendi menjelaskan, jatah vaksin untuk
+          Balikpapan adalah 8.900 dosis. Sebenarnya ini tadinya akan digunakan
+          untuk dua kali suntikan sebanyak 4.450 orang. "Tapi hasil webinar
+          Kepala Dinas Kesehatan (Dinkes) dengan pemerintah pusat, hampir 9
+          ribuan vaksin ini bisa digunakan untuk vaksin pertama dulu," terang
+          Wali Kota Rizal Effendi, Selasa (23/2/21). Untuk mempercepat
+          pemerataan vaksinasi, sebanyak 8.900 dosis ini akan digunakan sebagai
+          vaksin pertama untuk masyarakat yang terlah masuk data vaksinasi tahap
+          dua. "Kita sudah diberi petunjuk, vaksin pertama akan dilakukan untuk
+          45 orang anggota DPRD keseluruhan, pejabat negara, wali kota dan wakil
+          wali kota, tokoh agama, pedagang pasar, guru, keamanan, ASN, dan
+          lainnya yang sudah kita data," sebut Wali Kota Balikpapan. Jika
+          keseluruhan vaksin yang datang besok bisa disuntikkan sebagai vaksin
+          pertama, nanti persentasenya akan dihitung kembali. "Berapa untuk
+          masing-masing, misal berapa tokoh agama, berapa pedagang. Kita siapkan
+          tahap pertama untuk pedagang pasar klandasan dan pasar Pandansari
+          dulu," katanya. Dengan rencana ini, ia berharap pekan 4 di bulan
+          Februari vaksinasi sudah bisa dilaksanakan. "Kalau besok datang, kan
+          ke provinsi dulu. Mungkin pembagiannya sekitar satu atau dua hari,"
+          ujar Rizal. Jika kalau begitu kemungkinan vaksinasi tahap II termin II
+          ini sudah bisa dilakukan. Seperti diketahui di termin satu vaksinasi
+          dilakukan untuk tenaga kesehatan. "Kami berharap nanti kalau ada
+          panggilan vaksin dari satgas, dinas kesehatan maupun Puskesmas, agar
+          masyarakat datang. Karena nanti juga akan ada vaksinasi massal di
+          Dome," terangnya. Selain itu, keseluruhan ada 51 tempat vaksinasi,
+          yakni di fasilitas pelayanan kesehatan. Mulai Rumah Sakit, Puskesmas
+          hingga klinik," terangnya. Sementara untuk pedagang pasar, akan
+          mendapatkan vaksinasi di pasar. Nanti akan ada petugas atau tim yang
+          membuat tenda di pasar. Ditambahkannya juga, Dinkes Ini juga masih
+          menghitung siapa saja yang menjadi penerima vaksin tahap I termin II
+          ini. Seperti diketahui, mereka yang menjadi prioritas vaksin adalah
+          pekerja pelayanan publik. "Jadi kami diberi panduan, termasuk
+          keamanan, guru, pedagang, dan lainnya," sebutnya. Ia mengakui, memang
+          jumlah vaksin yang akan datang ini masih kurang untuk masyarakat
+          Balikpapan. Sehingga, jika nanti ada masyarakat yang menolak vaksin,
+          akan ditunda sementara. "Jadi kalau yang namanya masuk dalam daftar
+          tapi menolak vaksin, akan kami berikan pada yang lain dahulu. Kan
+          nanti bisa dilakukan edukasi lagi," katanya. Saat ini perjalanan
+          hingga vaksinasi merata untuk seluruh masyarakat masih sangat panjang.
+          Sehingga pihaknya akan mendahulukan yang bersedia divaksin. "Kalau
+          tidak mau ya yang lain dulu. Nanti dialihkan saja. Masih 9 ribuan,
+          padahal kebutuhan lebih 50 ribuan. Jadi tidak masalah. Tapi nanti
+          kalau sudah terakhir baru kami akan tentukan sikap bagaimana yang
+          benar-benar tidak mau divaksin," tuturnya. (diskominfo/
+          cha/editor:mgm)
+        </p>
 
-                            <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+        <v-btn-toggle
+          v-model="toggle_multiple"
+          color="primary"
+          dense
+          group
+          multiple
+        >
+          <v-btn :value="1" text>
+            <v-icon>mdi-format-bold</v-icon>
+          </v-btn>
 
-                            <v-card-actions>
-                                <v-btn text color="teal accent-4" @click="reveal = true">
-                                    Selengkapnya
-                                </v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-col>
-                </v-row>
-                <div class="text-center mt-10 mb-10">
-                    <v-btn color="primary" elevation="6" large>Lihat Berita Lainnya</v-btn>
-                </div>
-            </v-col>
+          <v-btn :value="2" text>
+            <v-icon>mdi-format-italic</v-icon>
+          </v-btn>
 
-            <v-col cols="12">
-                <div class="text-center mt-10">
-                    <h1>Protokol Kesehatan</h1>
-                    <hr style="width:150px; margin: auto;" class="mt-1 teal" />
-                </div>
+          <v-btn :value="3" text>
+            <v-icon>mdi-format-underline</v-icon>
+          </v-btn>
 
-                <div class="d-flex flex-column justify-space-between align-center">
-                    <v-img contain max-height="550" max-width="550"
-                        src="https://s3-alpha-sig.figma.com/img/154f/9173/00f6c976b3393f8eab4afe0912e9cefd?Expires=1670198400&Signature=MiF5DkCA~hmn5zo3-mlHrA5SjGGNtMLpxnJBb-MKXue1kh5WCBpIyvL9IL0jnkTHPfJboasytZlvDyDNSeuJJHrxgknvnEvTEctdqY2Gx-LvyCM~G84bbF2kak5T6MnE7yAsT0nmX5g-YDxn9zTw-~BrRH0FmIbwcZ15Muoiwy3UXEaXvzjCTQZ1rVTFPLaX1FrExb1P0NeMfP59DiNKlh3qFEW6QPH8HMzAIsD1WETPcvuza-1CP-TDNPrP8iw4k658OVEDwQ-WgDXL2BC-dBmPBBnzHG9b43bGDoLYTGt51ak16RLbGoDD-WqMKBJms-lJDCAyDI1g2XEwq~yPvw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
-                    </v-img>
-                </div>
-            </v-col>
+          <v-btn :value="4" text>
+            <v-icon>mdi-format-color-fill</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+        <v-divider></v-divider>
 
-            <v-col cols="12">
-                <div class="text-center mt-5 mb-10">
-                    <h1>Event</h1>
-                    <hr style="width:150px; margin: auto;" class="mt-1 teal" />
-                </div>
-                <v-row class="mx-5">
-                    <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
-                        <v-card>
-                            <v-img :src="card.src" class="white--text align-end"
-                                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px">
-                                <v-card-title v-text="card.title"></v-card-title>
-                            </v-img>
-
-                            <v-card-title> Top western road trips </v-card-title>
-
-                            <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
-
-                            <v-card-actions>
-                                <v-btn text color="teal accent-4" @click="reveal = true">
-                                    Selengkapnya
-                                </v-btn>
-                            </v-card-actions>
-                        </v-card>
-                    </v-col>
-                </v-row>
-                <div class="text-center mt-10 mb-10">
-                    <v-btn color="primary" elevation="6" large>Lihat Berita Lainnya</v-btn>
-                </div>
-            </v-col>
-
-            <v-col cols="12">
-                <div class="text-center mt-10 mb-10">
-                    <h1>Partner</h1>
-                    <hr style="width:150px; margin: auto;" class="mt-1 teal" />
-                </div>
-
-                <v-row class="mx-5">
-                    <v-col class="d-flex child-flex" cols="4">
-                        <v-img contain max-height="400" max-width="400"
-                            src="https://s3-alpha-sig.figma.com/img/08b1/9a7a/47a87e230718d373d5f9b030e94aa540?Expires=1670198400&Signature=KljdwGTK-8sj-oGIME865Bc6AKydMqsSGBGoBL7iTHRZSeLteGOUpwqGx3csFKH1Bh2NFDc2GHj69VpO0~YiTg5~rwP7PgUtRKtAlQPlCiTpZeKjWNatWYktFxluNHhirQzUm3glkqOyQ~ywMjhZ4KaE1lkSeIg6FAoshBlhMRuV1u7IFr1oVur2ept7mqexkJuabrzmvDwDnfpaJXYX5KT7wgqShBcbJ8lqgvhrgGE8h-zJwK6NHnvQZ52C9eqyW~O04Arl76pIHdsHS91hSUWV5sc~Hw~-q2ggZ0pouiNIaKtn77Ewu4napPoabHBu3mDQsYY7TbxjtCYgJ5qwsw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
-                        </v-img>
-                    </v-col>
-                    <v-col class="d-flex child-flex" cols="4">
-                        <v-img contain max-height="400" max-width="400"
-                            src="https://s3-alpha-sig.figma.com/img/c72b/564b/e91d4e0eca99f67bab46979b864a29a9?Expires=1670198400&Signature=IhaF4LXrY3BSa1Phwub0RYyfp3JcNw5PC0FOz8l0IYbYdCi1vY6OAFE7~VSB28advFlu4PJuRoEAS9mfRfoduKFNa9qqZvH4LOC330uh917u-dVWCKqistqldbOKkgq6qzdjms3HdOprDSTQS067RMwsaVrmuTHRX4Z2rO498YCPDP3cB6a04TZNKpbZ~~jBsGdp-T-vDKB3XmFRxhChLDptMfbnn5EclY3ZO2Tjn7V4ez4tiCvntgFmt8KEgPkqxyLSWfqZ1kov3O65KDLo6rYGCXp8MIrs1fdTz1MqNsR65O4QEq9zXrmDYY9WQoUAbUaiKtJ1RDCyiVuQAlfyHQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
-                        </v-img>
-                    </v-col>
-                    <v-col class="d-flex child-flex" cols="4">
-                        <v-img contain max-height="400" max-width="400"
-                            src="https://s3-alpha-sig.figma.com/img/c72b/564b/e91d4e0eca99f67bab46979b864a29a9?Expires=1670198400&Signature=IhaF4LXrY3BSa1Phwub0RYyfp3JcNw5PC0FOz8l0IYbYdCi1vY6OAFE7~VSB28advFlu4PJuRoEAS9mfRfoduKFNa9qqZvH4LOC330uh917u-dVWCKqistqldbOKkgq6qzdjms3HdOprDSTQS067RMwsaVrmuTHRX4Z2rO498YCPDP3cB6a04TZNKpbZ~~jBsGdp-T-vDKB3XmFRxhChLDptMfbnn5EclY3ZO2Tjn7V4ez4tiCvntgFmt8KEgPkqxyLSWfqZ1kov3O65KDLo6rYGCXp8MIrs1fdTz1MqNsR65O4QEq9zXrmDYY9WQoUAbUaiKtJ1RDCyiVuQAlfyHQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA">
-                        </v-img>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
-    </v-container>
+        <v-card class="mt-5" max-width="700" elevation="0">
+          <v-list-item three-line>
+            <v-list-item-avatar size="90" color="grey">
+              <v-img
+                src="https://s3-alpha-sig.figma.com/img/a250/b257/e4f8c1748c7fd8377b47858cd87b307f?Expires=1670198400&Signature=HwIkYYSF~HBINGANPSdiTdXQCDKSTG2cn~~Hf8gIV3vBc08-l7id050DwDue3poN3UAwpHmlLJClrxYNbG9DDDllM1iGjuyorq1cc2uNhRvdCFyigSUGu~z5yWHSg6qUjwMqMYOikmIexpdXSG6x3Wid5BLzp09ItjtpHXSXHVZLRskxw~iqpsJZ8Kkg0sxZIA4Xd4cKSgnZBgaPEhXrNR9MwwUZHx63bbDRcwk5ZavXMIBikopnpnugPY09kp0rsVwkDs75PuakhjWdtzj79RIN5hfi2wwOLm4udUphl-mcyaFe1nKKt~iYsvwQvWp~QmUcUFTSUFX-cWUGxATE~g__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+              ></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-subtitle class="cyan--text font-weight-black"
+                >Penulis</v-list-item-subtitle
+              >
+              <v-list-item-subtitle class="blue--text font-weight-bold mt-1"
+                >Nadia Sonya</v-list-item-subtitle
+              >
+              <v-list-item-content class="font-weight-light text-justify"
+                >Nadia Sonya adalah lulusan Teknik Informatika dari Binus
+                University yang saat ini menjadi salah satu Content Writer di
+                Jakarta Smart City. Gemar mengamati isu transportasi, olahraga,
+                teknologi dan sains, ia memulai karier Jurnalistik bersama media
+                internasional Motorsport.com pada 2016-2019. Saat ini ia
+                terfokus pada topik kesehatan, khususnya mengenai penanggulangan
+                pandemi Covid-19 di wilayah DKI Jakarta.</v-list-item-content
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   layout: 'default',
-  data: () => ({
-    cards: [
-      {
-        title: 'Favorite road trips',
-        src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-        flex: 4,
-      },
-      {
-        title: 'Best airlines',
-        src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-        flex: 4,
-      },
-      {
-        title: 'Best airlines',
-        src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-        flex: 4,
-      },
-    ],
-    items: [
-      {
-        src: 'https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2021/10/20/1040466642.jpg',
-      },
-      {
-        src: 'https://explore.makassarkota.go.id/wp-content/uploads/2022/03/DSC_1379-1-2048x1356.jpg',
-      },
-      {
-        src: 'https://asset.kompas.com/crops/UuJ3c5sXX0Y-4X3NRrAAWbKrpDI=/84x4:1139x532/750x500/data/photo/2020/02/05/5e3ac50f06746.jpeg',
-      },
-      {
-        src: 'https://portalbalikpapan.com/wp-content/uploads/2019/08/talkshow_baf_fair_2019.jpg',
-      },
-    ],
-    model: 0,
-    colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
-  }),
 }
 </script>
