c67517487338bd8d2491248c22a6fd66870cd330
Ulsyairil <ulsyairil@outlook.co.id>
2023-10-12 11:50:48 +0800

    Fix ecosystem for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'Forkohat Front',
+      name: 'forkohat-front',
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
+      instances: '1',
+      wait_ready: true,
+      autorestart: true,
+      max_restarts: 1,
+      watch: true,
+      error_file: 'logs/forkohat-front/err.log',
+      out_file: 'logs/forkohat-front/out.log',
+      log_file: 'logs/forkohat-front/combined.log',
+      time: true,
     },
   ],
 }
