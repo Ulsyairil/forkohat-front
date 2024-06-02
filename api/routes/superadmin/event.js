4612b571f1a76c0ab82e62764ba3e0474a6de4df
Ulsyairil <ulsyairil@outlook.co.id>
2024-06-02 09:57:38 +0800

    WIP: add API event comment superadmin

--- api/routes/superadmin/event.js
+++ api/routes/superadmin/event.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import rateLimit from '../../middleware/rateLimit.js'
 import { createEvent, deleteEvent, destroyEvent, editEvent, getEvent, listEvents, restoreEvent } from '../../controllers/superadmin/eventController.js'
+import { createEventComment, deleteEventComment, editEventComment, getEventComment, listEventComments } from '../../controllers/superadmin/eventCommentController.js'
 
 const router = Router()
 
@@ -11,5 +12,11 @@ router.post('/superadmin/event/edit', rateLimit, editEvent)
 router.post('/superadmin/event/restore', rateLimit, restoreEvent)
 router.post('/superadmin/event/dump', rateLimit, deleteEvent)
 router.post('/superadmin/event/delete', rateLimit, destroyEvent)
+
+router.post('/superadmin/event/comments', listEventComments)
+router.get('/superadmin/event/comment', getEventComment)
+router.post('/superadmin/event/comment/create', rateLimit, createEventComment)
+router.post('/superadmin/event/comment/edit', rateLimit, editEventComment)
+router.post('/superadmin/event/comment/delete', rateLimit, deleteEventComment)
 
 export default router
