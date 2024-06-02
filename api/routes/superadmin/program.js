cd8f7208e0a4b60d647948728efda3e8088f5f57
Ulsyairil <ulsyairil@outlook.co.id>
2024-06-02 09:24:22 +0800

    WIP: add API isi tatanan superadmin

--- api/routes/superadmin/program.js
+++ api/routes/superadmin/program.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import rateLimit from '../../middleware/rateLimit.js'
-import { createProgram, destroyProgram, editProgram, getProgram, programList } from '~/api/controllers/superadmin/programController.js'
-import { createStructure, destroyStructure, editStructure, getStructure, structureList } from '~/api/controllers/superadmin/structureController.js'
+import { createProgram, destroyProgram, editProgram, getProgram, programList } from '../../controllers/superadmin/programController.js'
+import { createStructure, destroyStructure, editStructure, getStructure, structureList } from '../../controllers/superadmin/structureController.js'
+import { createStructureItem, deleteStructureItem, editStructureItem, getStructureItem, listStructureItem, restoreStructureItem } from '../../controllers/superadmin/itemStructureController.js'
 
 const router = Router()
 
@@ -16,5 +17,13 @@ router.get('/superadmin/arrangement', getStructure)
 router.post('/superadmin/arrangement/create', rateLimit, createStructure)
 router.post('/superadmin/arrangement/edit', rateLimit, editStructure)
 router.post('/superadmin/arrangement/delete', rateLimit, destroyStructure)
+
+router.post('/superadmin/arrangement/items', listStructureItem)
+router.get('/superadmin/arrangement/item', getStructureItem)
+router.post('/superadmin/arrangement/item/create', rateLimit, createStructureItem)
+router.post('/superadmin/arrangement/item/edit', rateLimit, editStructureItem)
+router.post('/superadmin/arrangement/item/dump', rateLimit, deleteStructureItem)
+router.post('/superadmin/arrangement/item/restore', rateLimit, restoreStructureItem)
+router.post('/superadmin/arrangement/item/delete', rateLimit, deleteStructureItem)
 
 export default router
