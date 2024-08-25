5994b9e42d7619835566ac8ce0729b27c042082e
Ulsyairil <ulsyairil@outlook.co.id>
2024-08-25 21:10:25 +0800

    fix pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: 'forkohat-front',
+      name: 'forkohat-web',
       exec_mode: 'cluster',
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start',
-      instances: '1',
+      instances: 1,
+      script: 'node_modules/nuxt/bin/nuxt.js',
+      env_production: {
+        args: 'start',
+      },
+      env_development: {
+        script: '--watch',
+      },
       wait_ready: true,
       autorestart: true,
       max_restarts: 1,
-      watch: true,
-      error_file: 'logs/forkohat-front/err.log',
-      out_file: 'logs/forkohat-front/out.log',
-      log_file: 'logs/forkohat-front/combined.log',
+      watch: false,
       time: true,
+      error_file: '.logs/err.log',
+      out_file: '.logs/out.log',
+      merge_logs: false,
+      log_type: "json",
     },
   ],
 }
